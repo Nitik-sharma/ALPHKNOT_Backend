35ed914783f1c244ba6f6e6ebaa9eceb20807b29
Nitik Sharma <sharmanitik36@gmail.com>
2026-06-12 19:35:50 +0530

    Added CORS

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from "express";
+import cors from "cors"
 import bookCallRoutes from "./src/routes/bookCall.route.js";
 import talent from "./src/routes/hireTalent.route.js"
 import jobApplication from "./src/routes/jobApplication.route.js"
 import { configDotenv } from "dotenv";
 
 configDotenv();
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://13.49.78.176",
+      "https://alph-knot.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 const app = express();
 
 // middleware
@@ -18,7 +28,7 @@ app.use("/api/job-application",jobApplication)
 // test route
 app.get("/", (req, res) => {
   res.send("Server is running");
-});
+});no
 
 // start server
 const PORT = 5000;
