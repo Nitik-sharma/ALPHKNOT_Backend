fd89602acc8111f82df8c3c2318030de0131d4be
Nitik Sharma <sharmanitik36@gmail.com>
2026-07-16 11:24:10 +0530

    add nodeemailer

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,7 +5,7 @@ export const sendEmail = async (to, subject, html) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: process.env.EMAIL_PORT == 465, // true only for port 465
+      secure: process.env.EMAIL_PORT == 587, // true only for port 465
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
